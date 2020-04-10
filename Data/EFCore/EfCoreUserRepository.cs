1c9c3ee0ddd04f42a8c795d9c29f2d8aed746512
Kristi <kltakach@gmail.com>
2020-04-10 15:40:46 -0400

    adding user repository and migrations.

---
+++ Data/EFCore/EfCoreUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using codefucius_api.Models;
+
+namespace codefucius_api.Data.EFCore
+{
+    public class EfCoreUserRepository : EfCoreRepository<User, DataContext>
+    {
+        public EfCoreUserRepository(DataContext context) : base(context)
+        {
+
+        }
+    }
+}
