f4d4026542f8e1dcac23d4409c01e36d8b5ce8e2
Kristi <kltakach@gmail.com>
2020-04-10 15:22:01 -0400

    refactoring user to get working with base controller. tested GET POST PUT DELETE in postman on localhost:44363.

--- Startup.cs
+++ Startup.cs
@@ -30,6 +30,8 @@ namespace codefucius_api
             services.AddControllers();
 
             services.AddScoped<EfCoreReviewRepository>();
+            services.AddScoped<EfCoreUserRepository>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
