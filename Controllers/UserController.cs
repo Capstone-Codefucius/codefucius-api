fca313750b1c503f17a050e0f550888d28019782
Collin Werner <collinw9898@gmail.com>
2020-04-13 15:39:49 -0400

    Added dummy user data generator

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -10,7 +10,33 @@ namespace codefucius_api.Controllers
     {
         public UserController(EfCoreUserRepository repository) : base(repository)
         {
+            if (repository.GetAll().Result.Count == 0)
+            {
+                CreateDummyUsers(repository);
+            }
+        }
 
+        private void CreateDummyUsers(EfCoreUserRepository repository)
+        {
+            User user1 = new User();
+            user1.name = "Greg Mann";
+            user1.role = "Reviewee";
+            repository.Add(user1).Wait();
+
+            User user2 = new User();
+            user2.name = "Darwin Fish";
+            user2.role = "Reviewer";
+            repository.Add(user2).Wait();
+
+            User user3 = new User();
+            user3.name = "Bilbo Baggins";
+            user3.role = "Reviewee";
+            repository.Add(user3).Wait();
+
+            User user4 = new User();
+            user4.name = "Man dude";
+            user4.role = "Reviewer";
+            repository.Add(user4).Wait();
         }
     }
 }
